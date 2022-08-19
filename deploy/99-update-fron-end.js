7efaba0f4fc59abc84a2033f1de789d06be60498
Nirmalya Nayak <n.nayak263@gmail.com>
2022-08-19 17:20:03 +0530

    updated front-end deploy scripts

--- deploy/99-update-fron-end.js
+++ deploy/99-update-fron-end.js
@@ -1,29 +1,55 @@
-const { ethers, network } = require("hardhat")
-const fs = require('fs')
+const { ethers, network } = require("hardhat");
+const fs = require("fs");
 
-const frontEndContractsFile = '../fcc-nft-full-stack-nextjs-morails/nft-marketplace/constants/networkMapping.json'
+const frontEndContractsFile =
+  "../fcc-nft-full-stack-nextjs-morails/nft-marketplace/constants/networkMapping.json";
+const frontEndAbiLocation =
+  "../fcc-nft-full-stack-nextjs-morails/nft-marketplace/constants/";
 
-module.exports = async function() {
+module.exports = async function () {
   if (process.env.UPDATE_FRONT_END) {
-    console.log("Updating Front_End...")
+    console.log("Updating Front_End...");
 
-    await updateContractAddresses()
+    await updateContractAddresses();
+    await updateAbi();
+
+    console.log("Front end written!");
   }
+};
+
+async function updateAbi() {
+  const nftMarketplace = await ethers.getContract("NftMarketplace");
+  fs.writeFileSync(
+    `${frontEndAbiLocation}NftMarketplace.json`,
+    nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+  );
+
+  const basicNft = await ethers.getContract("BasicNft");
+  fs.writeFileSync(
+    `${frontEndAbiLocation}BasicNft.json`,
+    basicNft.interface.format(ethers.utils.FormatTypes.json)
+  );
 }
 
 async function updateContractAddresses() {
-  const nftMarketplace = await ethers.getContract("NftMarketplace")
-  const chainId = network.config.chainId.toString()
-  const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, 'utf8'))
+  const nftMarketplace = await ethers.getContract("NftMarketplace");
+  const chainId = network.config.chainId.toString();
+  const contractAddresses = JSON.parse(
+    fs.readFileSync(frontEndContractsFile, "utf8")
+  );
 
   if (chainId in contractAddresses) {
-    if (!contractAddresses[chainId]["NftMarketplace"].includes(nftMarketplace.address)) {
-      contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address)
+    if (
+      !contractAddresses[chainId]["NftMarketplace"].includes(
+        nftMarketplace.address
+      )
+    ) {
+      contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address);
     }
   } else {
-    contractAddresses[chainId] = {"NftMarketplace": [nftMarketplace.address]}
+    contractAddresses[chainId] = { NftMarketplace: [nftMarketplace.address] };
   }
-  fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
+  fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses));
 }
 
-module.exports.tags = ['all', 'frontend']
+module.exports.tags = ["all", "frontend"];
